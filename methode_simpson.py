5ef7716c6f97e79be3e8909892ef03f49d768ece
tenessy_de-faria <tenessy.de-faria@knap.fr>
2024-06-05 16:02:37 -0400

    Implémentation de l'intégration de simpson avec les 2 méthodes + calcul de l'erreur

--- methode_simpson.py
+++ methode_simpson.py
@@ -12,21 +12,34 @@ def calculer_nombre_echantillon(intervalle,n):
     return x
 
 def calculer_simpson(polynome,intervalle,n):
-    s = 0
-    h = (intervalle[1]-intervalle[0])/n
+    somme = 0
+    pas = (intervalle[1]-intervalle[0])/n
     x1 = intervalle[0]
-    x2 = intervalle[0] + h
+    x2 = intervalle[0] + pas
 
     for i in range(n):
-        s += (f(polynome,x1)+4*f(polynome,(x1+x2)/2)+f(polynome,x2))/6
-        x1 += h
-        x2 += h
-    return h*s
+        somme += (f(polynome,x1)+4*f(polynome,(x1+x2)/2)+f(polynome,x2))/6
+        x1 += pas
+        x2 += pas
+    return pas*somme
+
+def calculer_simpson_numpy(polynome, intervalle, n):
+    x = calculer_nombre_echantillon(intervalle, n)
+    y = f(polynome, x)
+    resultat = scipy.integrate.simpson(y, x=x)
+    return resultat
+
+def calculer_erreur(resulat1, resultat2):
+    erreur = abs(resulat1 - resultat2)
+    return erreur
 
 polynome = [1,1,1,1]
 intervalle = [0,1]
 n = 10000
-dx = calculer_nombre_echantillon(intervalle,n)
 
-print(calculer_simpson(polynome,intervalle,n))
-print(scipy.integrate.simpson(polynome, x=None, dx, axis=-1, even=<object object>))
+r1 = calculer_simpson(polynome,intervalle,n)
+r2 = calculer_simpson_numpy(polynome,intervalle,n)
+
+print(f"\nRésultat de l'intégration par la méthode de simpson en Python de base est de: {r2}")
+print(f"Résultat de l'intégration par à l'aide de NumPy: {r1}")
+print(f"Erreur = {calculer_erreur(r1, r2)}, soit : {(calculer_erreur(r1, r2)/abs(r1))*100} %")
