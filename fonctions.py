41edde05daeca0c698bbb392eadd8d8abb9a6104
Fabien Bertrand <fabien.btd25@gmail>
2024-06-05 15:45:21 -0400

    test 100 segments

--- fonctions.py
+++ fonctions.py
@@ -16,7 +16,7 @@ def integrer_analytique(polynome,interval):
 def integrer_methode_rectangle(polynome,nb_segment,interval):
     longeur_segment=((interval[1]-interval[0])/nb_segment)
     integration=0
-    for i in range(nb_segment-1):
+    for i in range(nb_segment):
         aire_rectangle=(calculer_valeur_polynome(polynome,(i+1/2)*longeur_segment))*longeur_segment
         integration+=aire_rectangle
     return integration
@@ -24,7 +24,3 @@ def integrer_methode_rectangle(polynome,nb_segment,interval):
 def calculer_erreur_integartion(polynome,nb_segment,interval):
     erreur_integartion=(integrer_analytique(polynome,interval)-integrer_methode_rectangle(polynome,nb_segment,interval))/integrer_analytique(polynome,interval)
     return (f"l'erreur d'intégration est de {erreur_integartion*100} %")
-
-print(integrer_analytique([1,1,1,1],[0,1]))
-print(integrer_methode_rectangle([1,1,1,1],100,[0,1]))
-print(calculer_erreur_integartion([1,1,1,1],100,[0,1]))
--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from fonctions import *
 
+#soit la fonction 1 + X + X2 + X3
+polynome=[float(input("X0")),float(input("X1")),float(input("X2")),float(input("X3"))]
+interval=[float(input("borne intégration inférieur")),float(input("borne intégration supérieur"))]
+print(f"l'intégration analytrique entre 0 et 1 nous donne {integrer_analytique(polynome,interval)}")
+nb_segment=int(input("nombre de segment pour l intégration avec méthode des rectangle"))
+print(f"l'intégration analytrique entre 0 et 1 nous donne {integrer_methode_rectangle(polynome,nb_segment,interval)}")
+print(f"l'erreur d'intégration est donc {calculer_erreur_integartion(polynome,nb_segment,interval)*100} % ")
