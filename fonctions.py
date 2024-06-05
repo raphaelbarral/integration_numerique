8ac52e8490a46ea7028ebe601dcdda39912612dc
raphael <raphael.barral.1@ens.etsmtl.ca>
2024-06-05 14:28:30 -0400

    Trapeze

--- fonctions.py
+++ fonctions.py
@@ -3,7 +3,11 @@ import matplotlib.pyplot as plt
 import timeit
 import time
 
-def integrer_methode_trapeze(polynome, nb_segment, interval)
+def integrer_methode_trapeze(polynome, nb_segment, interval):
     x = np.linspace(interval[0], interval[1], nb_segment+1)
-    y = x.map(calculer_valeur_polynome())
-    return 0
+    y = polynome[0] + polynome[1]*x + polynome[2]*x**2 + polynome[3]*x**3
+    T = ((x[1:]-x[:-1])*(y[1:]+y[:-1])/2).sum()
+
+    plt.plot(x, y)
+    plt.show()
+    return T
