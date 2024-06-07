22271eec9db4dff55396c5f2fe242dd82d163208
tenessy_de-faria <tenessy.de-faria@knap.fr>
2024-06-07 17:22:18 -0400

    Ajout de l'affichage de la précision de la méthode de simpson écrite en python de base

--- methode_simpson.py
+++ methode_simpson.py
@@ -8,18 +8,15 @@ temps = []
 nb_de_segment2 = []
 temps2 = []
 
-
 def f(polynome, x):
     valeur = 0
     for i in range(0, len(polynome)):
         valeur += polynome[i] * x ** (i)
     return valeur
 
-
 def calculer_nombre_echantillon(intervalle, n):
     x = np.linspace(intervalle[0], intervalle[1], n)
     return x
-
 
 def calculer_simpson(polynome, intervalle, n):
     somme = 0
@@ -32,18 +29,15 @@ def calculer_simpson(polynome, intervalle, n):
         x2 += pas
     return pas * somme
 
-
 def calculer_simpson_numpy(polynome, intervalle, n):
     x = calculer_nombre_echantillon(intervalle, n)
     y = f(polynome, x)
     resultat = scipy.integrate.simpson(y, x=x)
     return resultat
 
-
 def calculer_erreur(resulat1, resultat2):
     erreur = abs(resulat1 - resultat2)
     return erreur
-
 
 def calculer_temps_simpson_numpy(n, polynome, intervalle):
     global nb_de_segment2, temps2
@@ -68,9 +62,22 @@ def calculer_temps_simpson(n, polynome, intervalle):
         temps.append(toc - tic)
     return
 
+def calculer_valeur_polynome(polynome, x):
+    valeur = 0
+    for i in range(0,len(polynome)):
+        valeur+=polynome[i]*x**(i)
+    return valeur
 
-# Fonction pour afficher les résultats
-def afficher():
+def primitiver_polynome(polynome):
+    polynome_integre=[0]
+    for i in range(len(polynome)):
+        polynome_integre.append(polynome[i]/(i+1))
+    return polynome_integre
+
+def integrer_analytique(polynome,interval):
+    return(calculer_valeur_polynome(primitiver_polynome(polynome),interval[1])-calculer_valeur_polynome(primitiver_polynome(polynome),interval[0]))
+
+def afficher_temps_execution():
     plt.rcParams['figure.autolayout'] = True
     plt.rcParams['figure.dpi'] = 100
     plt.rcParams['font.size'] = 14
@@ -84,14 +91,39 @@ def afficher():
     plt.show()
     return
 
+def afficher_convergence_simpson(polynome, intervalle, n):
+
+    erreurs = []
+    nb_de_segments3 = []
+    for i in range(4, n, 2):
+        val_exact = integrer_analytique(polynome,intervalle)
+        err = calculer_erreur(val_exact, calculer_simpson_numpy(polynome, intervalle, i))
+        erreurs.append(err)
+        nb_de_segments3.append(i)
+
+    erreurs.pop(0)
+    nb_de_segments3.pop(0)
+
+    plt.rcParams['figure.autolayout'] = True
+    plt.rcParams['figure.dpi'] = 100
+    plt.rcParams['font.size'] = 14
+    plt.rcParams['figure.figsize'] = (12, 6)
+    plt.plot(nb_de_segments3,erreurs, label="Erreur d'intégration", color='blue')
+    plt.xlabel('Nombre de segments')
+    plt.ylabel("Valeur de l'erreur")
+    plt.title("Graphique montrant la convergence de la méthode de Simpson en fonction du nombre de segments")
+    plt.legend()
+    plt.show()
+    return
 
 polynome = [1, 1, 1, 1]
 intervalle = [0, 1]
 n = 1000
 
+afficher_convergence_simpson(polynome, intervalle, n)
 calculer_temps_simpson_numpy(n, polynome, intervalle)
 calculer_temps_simpson(n, polynome, intervalle)
-afficher()
+afficher_temps_execution()
 
 #print(f"\nRésultat de l'intégration par la méthode de simpson en Python de base est de: {r2}")
 #print(f"Résultat de l'intégration par à l'aide de NumPy: {r1}")
