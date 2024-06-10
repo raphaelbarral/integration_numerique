31f999513cd8eb0237798ce2d40ff63c0f83e1e0
raphael <raphael.barral.1@ens.etsmtl.ca>
2024-06-10 13:57:13 -0400

    V1.0

--- fonctions.py
+++ fonctions.py
@@ -1,5 +1,59 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import timeit
+import scipy
+
+
+def f(polynome, x):
+    valeur = 0
+    for i in range(0,len(polynome)):
+        valeur+=polynome[i]*x**(i)
+    return valeur
+
+def primitiver_polynome(polynome):
+    polynome_integre=[0]
+    for i in range(len(polynome)):
+        polynome_integre.append(polynome[i]/(i+1))
+    return polynome_integre
+
+def integrer_analytique(polynome,interval):
+    return(f(primitiver_polynome(polynome), interval[1]) - f(primitiver_polynome(polynome), interval[0]))
+
+
+def integrer_methode_simpson_scipy(polynome, n, intervalle):
+    x = np.linspace(intervalle[0], intervalle[1], n)
+    y = f(polynome, x)
+    resultat = scipy.integrate.simpson(y, x=x)
+    return resultat
+
+
+def integrer_methode_simpson_numpy(polynome, nb_segment, interval):
+    x = np.linspace(interval[0], interval[1], nb_segment + 1)
+    dx = (interval[1] - interval[0]) / nb_segment
+    x_mid = np.linspace(interval[0]+dx/2, interval[1]-dx/2, nb_segment)
+    y = polynome[0] + polynome[1] * x + polynome[2] * x ** 2 + polynome[3] * x ** 3
+    y_mid = polynome[0] + polynome[1] * x_mid + polynome[2] * x_mid ** 2 + polynome[3] * x_mid ** 3
+    T = ((x[1:] - x[:-1]) / 6 * (y[1:] + 4*y_mid + y[:-1])).sum()
+    return T
+
+
+def integrer_methode_simpson(polynome, n, intervalle):
+    somme = 0
+    pas = (intervalle[1] - intervalle[0]) / n
+    x1 = intervalle[0]
+    x2 = intervalle[0] + pas
+    for i in range(n):
+        somme += (f(polynome, x1) + 4 * f(polynome, (x1 + x2) / 2) + f(polynome, x2)) / 6
+        x1 += pas
+        x2 += pas
+    return pas * somme
+
+
+def integrer_methode_trapeze_scipy(polynome, n, intervalle):
+    x = np.linspace(intervalle[0], intervalle[1], n)
+    y = f(polynome, x)
+    resultat = scipy.integrate.trapezoid(y, x=x)
+    return resultat
 
 def integrer_methode_trapeze_numpy(polynome, nb_segment, interval):
     x = np.linspace(interval[0], interval[1], nb_segment + 1)
@@ -13,33 +67,8 @@ def integrer_methode_trapeze(polynome, nb_segment, interval):
     x0 = interval[0]
     T = 0
     for i in range(nb_segment):
-        T += dx * (calculer_valeur_polynome(polynome, x0 + i * dx) + calculer_valeur_polynome(polynome,
-                                                                                              x0 + (i + 1) * dx)) / 2
+        T += dx * (f(polynome, x0 + i * dx) + f(polynome, x0 + (i + 1) * dx)) / 2
     return T
-
-
-def calculer_valeur_polynome(polynome, x):
-    valeur = 0
-    for i in range(0,len(polynome)):
-        valeur+=polynome[i]*x**(i)
-    return valeur
-
-def primitiver_polynome(polynome):
-    polynome_integre=[0]
-    for i in range(len(polynome)):
-        polynome_integre.append(polynome[i]/(i+1))
-    return polynome_integre
-
-def integrer_analytique(polynome,interval):
-    return(calculer_valeur_polynome(primitiver_polynome(polynome),interval[1])-calculer_valeur_polynome(primitiver_polynome(polynome),interval[0]))
-
-def integrer_methode_rectangle(polynome,nb_segment,interval):
-    longeur_segment=(interval[1]-interval[0])/nb_segment
-    integration=0
-    for i in range(nb_segment):
-        aire_rectangle=(calculer_valeur_polynome(polynome,interval[0]+(i+1/2)*longeur_segment))*longeur_segment
-        integration+=aire_rectangle
-    return integration
 
 
 def integrer_methode_rectangle_numpy(polynome, nb_segment, interval):
@@ -49,9 +78,55 @@ def integrer_methode_rectangle_numpy(polynome, nb_segment, interval):
     T = (dx*y).sum()
     return T
 
+def integrer_methode_rectangle(polynome,nb_segment,interval):
+    longeur_segment=(interval[1]-interval[0])/nb_segment
+    integration=0
+    for i in range(nb_segment):
+        aire_rectangle= (f(polynome, interval[0] + (i + 1 / 2) * longeur_segment)) * longeur_segment
+        integration+=aire_rectangle
+    return integration
+
 
 def calculer_erreur_integartion(polynome, nb_segment, interval, fonction_integral):
     erreur_integartion = (integrer_analytique(polynome, interval) - fonction_integral(polynome, nb_segment,
                                                                                                interval)) / integrer_analytique(
         polynome, interval)
     return abs(erreur_integartion)
+
+def plot_init():
+    plt.rcParams['figure.autolayout'] = True
+    plt.rcParams['figure.dpi'] = 100
+    plt.rcParams['font.size'] = 10
+    plt.rcParams['figure.figsize'] = (12, 6)
+
+def plot_ajouter_temps(integrer, polynome, interval, label, color):
+    temps = []
+    nb_seg = []
+    for i in range(10):
+        nb_seg.append(2 ** (i + 5))
+        temps.append(timeit.timeit(lambda: integrer(polynome, nb_seg[i], interval), number=10) / 10)
+    plt.plot(nb_seg, temps, label=label, color=color)
+
+def plot_afficher_temps(titre):
+    plt.yscale('log')
+    plt.xlabel('Nombre de segments')
+    plt.ylabel('Temps (secondes)')  # Ajout d'un label pour l'axe des ordonnées
+    plt.title(titre)
+    plt.legend()
+    plt.show()
+
+def plot_ajouter_erreur(integrer, polynome, interval, label, color):
+    erreur = []
+    nb_seg = []
+    for i in range(100):
+        nb_seg.append(i + 1)
+        erreur.append(calculer_erreur_integartion(polynome, nb_seg[i], interval, integrer))
+    plt.plot(nb_seg, erreur, label=label, color=color)
+
+def plot_afficher_erreur(titre):
+    plt.yscale('log')
+    plt.xlabel('Nombre de segments')
+    plt.ylabel("Valeur de l'erreur en %")
+    plt.title(titre)
+    plt.legend()
+    plt.show()
--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import timeit
-
-import numpy as np
-import matplotlib.pyplot as plt
 from fonctions import *
-from methode_simpson import *
+
+plot_init()
 
 #Partie méthode des rectangles
 #polynome=[float(input("X0")),float(input("X1")),float(input("X2")),float(input("X3"))]
@@ -21,16 +18,10 @@ print(f"l'intégration avec la méthode des rectangles nous donne {integrer_meth
 #Question 2.1.1.3
 print(f"l'erreur d'intégration est donc {calculer_erreur_integartion(polynome,nb_segment,interval, integrer_methode_rectangle)} % ")
 #Question 2.1.1.5
-results_rect = []
-nb_seg_rect = []
-for i in range(10):
-    nb_seg_rect.append(2 ** (i + 1))
-    results_rect.append(calculer_erreur_integartion(polynome, nb_seg_rect[i], interval, integrer_methode_rectangle))
 
-plt.plot(nb_seg_rect, results_rect)
-plt.yscale('log')
-plt.xscale('log')
-plt.show()
+plot_ajouter_erreur(integrer_methode_rectangle, polynome, interval, 'Python de base', 'blue')
+plot_afficher_erreur('Graphique montrant la convergence de la méthode des rectangle en fonction du nombre de segments')
+
 #Question 2.1.1.6
 print(f"Le temps d'exécution est {timeit.timeit(lambda :integrer_methode_rectangle(polynome, nb_segment, interval), number=1000)/1000} secondes pour la méthode des rectangles")
 
@@ -38,21 +29,9 @@ print(f"Le temps d'exécution est {timeit.timeit(lambda :integrer_methode_rectan
 print(f"l'intégration avec la méthode des rectangles numpy nous donne {integrer_methode_rectangle_numpy(polynome,nb_segment,interval)}")
 print(f"Le temps d'exécution est {timeit.timeit(lambda :integrer_methode_rectangle_numpy(polynome, nb_segment, interval), number=1000)/1000} secondes pour la méthode des rectangles numpy")
 
-temps_rect_numpy = []
-temps_rect = []
-nb_seg = []
-for i in range(10):
-    nb_seg.append(2 ** (i + 1))
-    temps_rect.append(timeit.timeit(lambda :integrer_methode_rectangle(polynome, nb_seg[i], interval), number=1000)/1000)
-    temps_rect_numpy.append(timeit.timeit(lambda :integrer_methode_rectangle_numpy(polynome, nb_seg[i], interval), number=1000)/1000)
-
-results = np.array([temps_rect , temps_rect_numpy]).transpose()
-nb_seg = np.array([nb_seg, nb_seg]).transpose()
-
-plt.plot(nb_seg, results)
-plt.yscale('log')
-plt.xscale('log')
-plt.show()
+plot_ajouter_temps(integrer_methode_rectangle, polynome, interval, 'Python de base', 'blue')
+plot_ajouter_temps(integrer_methode_rectangle_numpy, polynome, interval, 'numPy', 'red')
+plot_afficher_temps("Temps d'execution de la méthode des rectangle en fonction du nombre de segments")
 
 #Question 2.2.1
 print(f"l'intégration avec la méthode des trapezes nous donne {integrer_methode_trapeze(polynome,nb_segment,interval)}")
@@ -66,76 +45,32 @@ print(f"Le temps d'exécution est {timeit.timeit(lambda :integrer_methode_trapez
 print(f"Le temps d'exécution est {timeit.timeit(lambda :integrer_methode_trapeze_numpy(polynome, nb_segment, interval), number=1000)/1000} secondes pour la méthode des trapezes numpy")
 
 #Question 2.2.3
-results_trap = []
-nb_seg_trap = []
-for i in range(10):
-    nb_seg_trap.append(2 ** (i + 1))
-    results_trap.append(calculer_erreur_integartion(polynome, nb_seg_trap[i], interval, integrer_methode_trapeze))
-
-results = np.array([results_rect , results_trap]).transpose()
-nb_seg = np.array([nb_seg_rect, nb_seg_trap]).transpose()
-
-plt.plot(nb_seg, results)
-plt.yscale('log')
-plt.xscale('log')
-plt.show()
+plot_ajouter_erreur(integrer_methode_trapeze, polynome, interval, 'Python de base', 'blue')
+plot_afficher_erreur('Graphique montrant la convergence de la méthode des trapeze en fonction du nombre de segments')
 
 #Question 2.2.4
-temps_trap_numpy = []
-temps_trap = []
-nb_seg = []
-for i in range(10):
-    nb_seg.append(2 ** (i + 1))
-    temps_trap.append(timeit.timeit(lambda :integrer_methode_trapeze(polynome, nb_seg[i], interval), number=1000)/1000)
-    temps_trap_numpy.append(timeit.timeit(lambda :integrer_methode_trapeze_numpy(polynome, nb_seg[i], interval), number=1000)/1000)
-
-results = np.array([temps_trap , temps_trap_numpy]).transpose()
-nb_seg = np.array([nb_seg, nb_seg]).transpose()
-
-plt.plot(nb_seg, results)
-plt.yscale('log')
-plt.xscale('log')
-plt.show()
+plot_ajouter_temps(integrer_methode_trapeze, polynome, interval, 'Python de base', 'blue')
+plot_ajouter_temps(integrer_methode_trapeze_numpy, polynome, interval, 'numPy', 'red')
+plot_ajouter_temps(integrer_methode_trapeze_scipy, polynome, interval, 'méthode pré-programmé', 'green')
+plot_afficher_temps("Temps d'execution de la méthode des trapeze en fonction du nombre de segments")
 
 #Question 2.3.1
-print(f"l'intégration avec la méthode de Simpson nous donne {calculer_simpson(polynome,nb_segment,interval)}")
-print(f"l'intégration avec la méthode de Simpson numpy nous donne {calculer_simpson_numpy(polynome,nb_segment,interval)}")
+print(f"l'intégration avec la méthode de Simpson nous donne {integrer_methode_simpson(polynome,nb_segment,interval)}")
+print(f"l'intégration avec la méthode de Simpson numpy nous donne {integrer_methode_simpson_numpy(polynome,nb_segment,interval)}")
 
 #Question 2.3.2
-print(f"l'erreur avec la méthode de Simpson est {calculer_erreur_integartion(polynome,nb_segment,interval, calculer_simpson)} % ")
-print(f"l'erreur avec la méthode des Simpson numpy est {calculer_erreur_integartion(polynome,nb_segment,interval, calculer_simpson_numpy)} % ")
+print(f"l'erreur avec la méthode de Simpson est {calculer_erreur_integartion(polynome,nb_segment,interval, integrer_methode_simpson)} % ")
+print(f"l'erreur avec la méthode des Simpson numpy est {calculer_erreur_integartion(polynome,nb_segment,interval, integrer_methode_simpson_numpy)} % ")
 
-print(f"Le temps d'exécution est {timeit.timeit(lambda :calculer_simpson(polynome, nb_segment, interval), number=1000)/1000} secondes pour la méthode de Simpson")
-print(f"Le temps d'exécution est {timeit.timeit(lambda :calculer_simpson_numpy(polynome, nb_segment, interval), number=1000)/1000} secondes pour la méthode de Simpson")
+print(f"Le temps d'exécution est {timeit.timeit(lambda :integrer_methode_simpson(polynome, nb_segment, interval), number=1000)/1000} secondes pour la méthode de Simpson")
+print(f"Le temps d'exécution est {timeit.timeit(lambda :integrer_methode_simpson_numpy(polynome, nb_segment, interval), number=1000)/1000} secondes pour la méthode de Simpson")
 
-#Question 2.2.3
-results_simp = []
-nb_seg_simp= []
-for i in range(10):
-    nb_seg_simp.append(2 ** (i + 1))
-    results_simp.append(calculer_erreur_integartion(polynome, nb_seg_trap[i], interval, calculer_simpson))
+#Question 2.3.3
+plot_ajouter_erreur(integrer_methode_simpson, polynome, interval, 'Python de base', 'blue')
+plot_afficher_erreur('Graphique montrant la convergence de la méthode de Simpson en fonction du nombre de segments')
 
-results = np.array([results_rect , results_trap, results_simp]).transpose()
-nb_seg = np.array([nb_seg_rect, nb_seg_trap, nb_seg_simp]).transpose()
-
-plt.plot(nb_seg, results)
-plt.yscale('log')
-plt.xscale('log')
-plt.show()
-
-#Question 2.2.4
-temps_simp_numpy = []
-temps_simp = []
-nb_seg = []
-for i in range(10):
-    nb_seg.append(2 ** (i + 1))
-    temps_simp.append(timeit.timeit(lambda :calculer_simpson(polynome, nb_seg[i], interval), number=1000)/1000)
-    temps_simp_numpy.append(timeit.timeit(lambda :calculer_simpson_numpy(polynome, nb_seg[i], interval), number=1000)/1000)
-
-results = np.array([temps_trap , temps_trap_numpy]).transpose()
-nb_seg = np.array([nb_seg, nb_seg]).transpose()
-
-plt.plot(nb_seg, results)
-plt.yscale('log')
-plt.xscale('log')
-plt.show()
+#Question 2.3.4
+plot_ajouter_temps(integrer_methode_simpson, polynome, interval, 'Python de base', 'blue')
+plot_ajouter_temps(integrer_methode_simpson_numpy, polynome, interval, 'numPy', 'red')
+plot_ajouter_temps(integrer_methode_simpson_scipy, polynome, interval, 'methode pré-programmé', 'green')
+plot_afficher_temps("Temps d'execution de la méthode de Simpson en fonction du nombre de segments")
